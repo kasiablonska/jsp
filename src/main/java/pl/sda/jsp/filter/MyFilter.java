cce9be06795dfa5c88868cc9d23f3c6f3c05f128
Kasia <kasiablonsk@op.pl>
2020-03-07 11:28:54 +0100

    zadanie Servlet 24

---
+++ src/main/java/pl/sda/jsp/filter/MyFilter.java
@@ -0,0 +1,25 @@
+package pl.sda.jsp.filter;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        final PrintWriter writer= response.getWriter();
+        writer.write("<html><body><div style='text-algin: center;'>");
+        final String login =request.getParameter("login");
+
+        if(login!=null && login.equals("admin")){
+            chain.doFilter(request, response);
+        }else {
+            writer.print("<p style='color: red; font-size: larger'>"+
+                    "User ID is invalid. Plase try again..."+
+                    "</p>");
+        }
+        writer.write("</div></body></html>");
+        writer.close();
+    }
+}
